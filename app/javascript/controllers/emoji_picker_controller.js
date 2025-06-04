85e8dd89029d89ecaba48957bd86e9d6949cf389
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2025-06-04 17:06:38 +0900

    improve emoji-picker

--- app/javascript/controllers/emoji_picker_controller.js
+++ app/javascript/controllers/emoji_picker_controller.js
@@ -5,7 +5,13 @@ export default class extends Controller {
   static targets = ['button', 'tooltip', 'form']
 
   connect() {
-    Popper.createPopper(this.buttonTarget, this.tooltipTarget)
+    this.popper = Popper.createPopper(this.buttonTarget, this.tooltipTarget)
+    document.addEventListener('click', this.#handleDocumentClick)
+  }
+
+  disconnect() {
+    this.popper.destroy()
+    document.removeEventListener('click', this.#handleDocumentClick)
   }
 
   toggle() {
@@ -15,10 +21,29 @@ export default class extends Controller {
   pick(event) {
     const { detail } = event
     this.#submit(detail)
+    this.#hide()
+  }
+
+  #hide() {
+    this.tooltipTarget.classList.remove('shown')
+  }
+
+  get #isShown() {
+    return this.tooltipTarget.classList.contains('shown')
   }
 
   #submit(emoji) {
     this.formTarget.emoji.value = JSON.stringify(emoji)
     this.formTarget.requestSubmit()
+  }
+
+  #handleDocumentClick = (event) => {
+    if (this.#isOutside(event))
+      this.#hide()
+  }
+
+  #isOutside(event) {
+    const { target } = event
+    return !this.buttonTarget.contains(target) && !this.tooltipTarget.contains(target)
   }
 }
