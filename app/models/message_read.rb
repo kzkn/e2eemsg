03443200a572e0b0de7f138ab50d46d945977c36
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2025-06-04 17:53:00 +0900

    message_read realtime update

--- app/models/message_read.rb
+++ app/models/message_read.rb
@@ -4,5 +4,14 @@ class MessageRead < ApplicationRecord
   belongs_to :message
   belongs_to :membership
 
-  # TODO: 既読も stream でつけるようにしたい
+  after_commit :broadcast_update
+
+  private
+
+  def broadcast_update
+    room = message.room
+    message.room.memberships.each do |membership|
+      broadcast_update_to membership, target: dom_id(message, :reads), html: room.read_count_text(message, membership)
+    end
+  end
 end
